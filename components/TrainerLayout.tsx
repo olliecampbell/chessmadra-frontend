6c5ac52dbaadc9c35d122eccb5735767650cc35a
Marcus <marcusbuffett@me.com>
2022-01-22 08:58:45 -0700

    .

--- components/TrainerLayout.tsx
+++ components/TrainerLayout.tsx
@@ -4,44 +4,44 @@ import {
   Pressable,
   useWindowDimensions,
   View,
-  Modal
-} from 'react-native'
-import React from 'react'
-import { c, s } from 'app/styles'
-import { Spacer } from 'app/Space'
-import { NavBar } from 'app/components/NavBar'
-import { useIsMobile } from 'app/utils/isMobile'
-import { intersperse } from '../utils/intersperse'
+  Modal,
+} from "react-native";
+import React from "react";
+import { c, s } from "app/styles";
+import { Spacer } from "app/Space";
+import { NavBar } from "app/components/NavBar";
+import { useIsMobile } from "app/utils/isMobile";
+import { intersperse } from "../utils/intersperse";
 
 export const TrainerLayout = ({ chessboard, children }: any) => {
-  const isMobile = useIsMobile()
+  const isMobile = useIsMobile();
   const icons = [
     {
-      icon: 'fa fa-twitter',
-      link: 'https://twitter.com/intent/tweet?url=https%3A%2F%2Fchessmadra.com&text=Check%20out%20this%20chess%20visualization%20site%20by%20%40marcusbuffett'
+      icon: "fa fa-twitter",
+      link: "https://twitter.com/intent/tweet?url=https%3A%2F%2Fchessmadra.com&text=Check%20out%20this%20chess%20visualization%20site%20by%20%40marcusbuffett",
     },
-    { icon: 'fa fa-envelope', link: 'mailto:me@mbuffett.com' },
+    { icon: "fa fa-envelope", link: "mailto:me@mbuffett.com" },
     {
-      icon: 'fa fa-github',
-      link: 'https://github.com/marcusbuffett/chess-trainer-site'
-    }
-  ]
+      icon: "fa fa-github",
+      link: "https://github.com/marcusbuffett/chess-trainer-site",
+    },
+  ];
   return (
-    <View style={s(c.column, c.minHeight('100vh'))}>
+    <View style={s(c.column, c.minHeight("100vh"))}>
       <NavBar />
       <View
         style={{
           flex: 1,
-          justifyContent: 'flex-start',
-          alignItems: 'center',
-          backgroundColor: 'none'
+          justifyContent: "flex-start",
+          alignItems: "center",
+          backgroundColor: "none",
         }}
       >
         <View
           style={s(
             c.fullWidth,
             !isMobile && c.center,
-            !isMobile && c.minWidth('100vw'),
+            !isMobile && c.minWidth("100vw"),
             !isMobile && c.my(48),
             isMobile && c.px(10),
             isMobile && c.pt(10)
@@ -55,11 +55,20 @@ export const TrainerLayout = ({ chessboard, children }: any) => {
                   isMobile ? c.column : s(c.row, c.alignCenter)
                 )}
               >
-                <View style={s(c.width(500), c.maxWidth('100%'))}>
+                <View style={s(c.width(500), c.maxWidth("100%"))}>
                   {chessboard}
                 </View>
                 <Spacer height={12} width={24} isMobile={isMobile} />
-                <View style={s(c.column, c.width(400))}>{children}</View>
+                <View
+                  style={s(
+                    c.column,
+                    c.width(400),
+                    c.maxWidth("100%"),
+                    isMobile && s(c.fullWidth, c.width(500))
+                  )}
+                >
+                  {children}
+                </View>
               </View>
             </>
           ) : (
@@ -86,13 +95,13 @@ export const TrainerLayout = ({ chessboard, children }: any) => {
                   className={`fas ${icon.icon}`}
                 ></i>
               </a>
-            )
+            );
           }),
           (i) => {
-            return <Spacer key={i} width={24} />
+            return <Spacer key={i} width={24} />;
           }
         )}
       </View>
     </View>
-  )
-}
+  );
+};
