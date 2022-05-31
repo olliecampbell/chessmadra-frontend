6f59dbbdc397b2595a77cc95d6125ba1930ec1a0
Marcus <marcusbuffett@me.com>
2022-05-30 20:48:15 -0400

    Alerting!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,23 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import AuthHandler from "app/components/AuthHandler";
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://5c4df4321e7b4428afef85ec9f08cbd1@o1268497.ingest.sentry.io/6456185",
+  integrations: [new BrowserTracing()],
+  environment:
+    !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+      ? "development"
+      : "production",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   const [isMounted, setIsMounted] = useState(false);
   console.log("Using the app!");
