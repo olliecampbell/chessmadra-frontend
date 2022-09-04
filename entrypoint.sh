6a47a3be53e4c4ef405866e66a7db2ac1fb3c023
Marcus <marcusbuffett@me.com>
2022-09-05 00:49:44 +0200

    Better config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-static-web-server --help
-SERVER_PORT=80 static-web-server --page-fallback /public/index.html --cache-control-headers=false -w /static-web-server.toml
+SERVER_PORT=80 static-web-server -w /static-web-server.toml
