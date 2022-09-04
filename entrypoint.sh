0f985b06f43cc5713cb98cd8b3d478a5f13c12fc
Marcus <marcusbuffett@me.com>
2022-09-04 22:38:39 +0200

    Disable cache for now

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 static-web-server --help
-SERVER_PORT=80 static-web-server --port 80 --page-fallback /public/index.html
+SERVER_PORT=80 static-web-server --port 80 --page-fallback /public/index.html --cache-control-headers=false
