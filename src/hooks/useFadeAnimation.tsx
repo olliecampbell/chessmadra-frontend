34e62b1ddb7377a8f921867d2f8a89df66b1df82
Marcus <marcusbuffett@me.com>
2022-10-16 00:21:31 -0600

    Fade animation hook

---
+++ src/hooks/useFadeAnimation.tsx
@@ -0,0 +1,18 @@
+import { Animated } from "react-native";
+import { useEffect, useRef } from "react";
+import React from "react";
+
+export const useFadeAnimation = (
+  visible: boolean,
+  options: { duration: number }
+) => {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  React.useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: visible ? 1 : 0,
+      duration: options.duration ?? 300,
+      useNativeDriver: false,
+    }).start();
+  }, [visible]);
+  return { fadeStyling: fadeAnim };
+};
