d6c921dbc36e6d8c7aa1bbc2bedaa7359f58c283
Marcus <marcusbuffett@me.com>
2022-11-28 13:32:25 -0500

    Lazy load oops

---
+++ src/components/LazyLoad.tsx
@@ -0,0 +1,31 @@
+import React, { useRef, useState } from "react";
+import { Animated, Easing, Pressable, View } from "react-native";
+// import { ExchangeRates } from "app/ExchangeRate";
+import { c, s } from "app/styles";
+import { Spacer } from "app/Space";
+import { CMText } from "./CMText";
+import { quick, useUserState } from "app/utils/app_state";
+import { THRESHOLD_OPTIONS } from "./ProfileModal";
+import { useResponsive } from "app/utils/useResponsive";
+import { getRecommendedMissThreshold } from "app/utils/user_state";
+import { useOutsideClick } from "app/components/useOutsideClick";
+import { SelectOneOf } from "./SelectOneOf";
+import { useOnScreen } from "app/utils/useIntersectionObserver";
+
+export const LazyLoad = ({
+  children,
+  style,
+}: {
+  children: any;
+  maxOpacity?: number;
+  style: any;
+}) => {
+  const ref = useRef(null);
+  const onScreen = useOnScreen(ref, "300px");
+
+  return (
+    <View ref={ref} style={s(style)}>
+      {onScreen && children}
+    </View>
+  );
+};
