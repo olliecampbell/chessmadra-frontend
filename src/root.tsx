5bf924621daefedbc237b745b320da2a11651ace
Marcus <marcusbuffett@me.com>
2023-04-11 16:01:23 -0600

    manifest and stuff

--- src/root.tsx
+++ src/root.tsx
@@ -98,6 +98,8 @@ export default function Root() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <link rel="manifest" href="manifest.json" />
+
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
