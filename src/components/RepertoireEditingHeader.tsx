c577ca1f7d60434bb016770d16221aa12c2e67fe
Marcus Buffett <me@mbuffett.com>
2023-06-17 10:39:49 +0200

    .

--- src/components/RepertoireEditingHeader.tsx
+++ src/components/RepertoireEditingHeader.tsx
@@ -3,9 +3,8 @@ import { BP, useResponsive } from "~/utils/useResponsive";
 import { CMText } from "./CMText";
 
 export const SidebarHeader = (props: { children: any }) => {
-  const responsive = useResponsive();
   return (
-    <CMText class=" text-primary mt-0 text-lg font-bold lg:-mt-2 lg:text-xl">
+    <CMText class="text-primary mt-0 text-xl font-semibold lg:-mt-2 lg:text-2xl">
       {props.children}
     </CMText>
   );
