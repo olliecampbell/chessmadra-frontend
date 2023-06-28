4befab0ada137a0166d4dbe594c0d79a71d3c8ce
Marcus Buffett <me@mbuffett.com>
2023-06-29 01:40:57 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import devtools from "solid-devtools/vite";
 import UnoCSS from "unocss/vite";
 import { fileURLToPath } from "node:url";
-import { unoConfig } from "~/utils/uno";
+import { unoConfig } from "./src/utils/uno";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 const IS_STORYBOOK = !!process.env.IS_STORYBOOK;
