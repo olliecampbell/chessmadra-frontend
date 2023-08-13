0015a74b830a3e9e025e009b1ecb8d3c5678f07b
Marcus Buffett <me@mbuffett.com>
2023-08-13 12:15:26 +0200

    .

--- src/routes/stupactylo.tsx
+++ src/routes/stupactylo.tsx
@@ -1,4 +1,12 @@
 import { onMount } from "solid-js";
+import { quick } from "~/utils/app_state";
 import { PageWrapper } from "~/components/PageWrapper";
+import { useNavigate } from "solid-start";
 
-export default PageWrapper;
+export default () => {
+  const navigate = useNavigate();
+  onMount(() => {
+    navigate("/");
+  });
+  return <PageWrapper />;
+};
