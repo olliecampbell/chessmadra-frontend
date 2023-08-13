42474a18bbb6135dd5d8bb91c4c54cd03d9b3c5a
Marcus Buffett <me@mbuffett.com>
2023-08-13 02:34:32 +0200

    .

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -9,16 +9,23 @@ import { Component, createEffect, Match, onMount, Switch } from "solid-js";
 import LandingPageWrapper from "~/components/LandingPageWrapper";
 import { AuthStatus } from "~/utils/user_state";
 import { RepertoireBuilder } from "./RepertoireBuilder";
+import { useLocation } from "solid-start";
+import { identify, Identify } from "@amplitude/analytics-browser";
 
 export const PageWrapper = (props: { initialView: Component }) => {
   const [userState] = useAppState((s) => [s.userState]);
-  const authState = () => getAppState().userState.authStatus;
   const token = () => getAppState().userState.token;
-  const [mode] = useSidebarState(([s]) => [s.mode]);
   const [repertoireLoading] = useRepertoireState((s) => [
     s.repertoire === undefined,
   ]);
   const [authStatus] = useAppState((s) => [s.userState.authStatus]);
+  const location = useLocation();
+
+  onMount(() => {
+    const identifyObj = new Identify();
+    identifyObj.setOnce("initial_page", location.pathname);
+    identify(identifyObj);
+  });
 
   createEffect(() => {
     if (repertoireLoading() && authStatus() === AuthStatus.Authenticated) {
---
+++ src/routes/stupactylo.tsx
@@ -0,0 +1,4 @@
+import { onMount } from "solid-js";
+import { PageWrapper } from "~/components/PageWrapper";
+
+export default PageWrapper;
