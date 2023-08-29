58632672abee1535dd7988ed3f8a73dcb9995b0e
Marcus Buffett <me@mbuffett.com>
2023-08-29 23:02:52 +0200

    colors?

--- src/routes/design-system.tsx
+++ src/routes/design-system.tsx
@@ -1,12 +1,12 @@
-import { For } from "solid-js"
+import { For, Show } from "solid-js"
 
 export default () => {
   return (
       <div class="w-full min-h-screen bg-gray-14 p-20 flex center">
       <div class="flex row">
       <Swatches hue="gray" step={5}/>
-      <div class="flex row ml-4 space-x-4">
-      <Swatches hue="red" step={10}/>
+      <div class="flex row ml-4 space-x-1">
+      <Swatches hue="red" step={10} showShade/>
       <Swatches hue="green" step={10}/>
       <Swatches hue="blue" step={10}/>
       <Swatches hue="orange" step={10}/>
@@ -20,15 +20,21 @@ export default () => {
       )
 }
 
-const ColorSwatch = ({ className }: { className?: string }) => {
+const ColorSwatch = ({ className, showShade }: { className?: string }) => {
   return (
       <div class="flex row items-center justify-between font-semibold">
-      {className.split("-")[1]} {className.split("-")[2]} <div class={`ml-4 w-20 h-8 ${className} rounded`}/>
+      <Show when={showShade}>
+
+      <div class={`mr-2`}>
+      {className.split("-")[2]} 
+      </div>
+      </Show>
+      <div class={`w-20 h-8 ${className} rounded`}/>
       </div>
       )
 }
 
-const Swatches = ({hue, step}: {hue: string, step: number}) => {
+const Swatches = ({hue, step, showShade}: {hue: string, step: number}) => {
 
 const shades = []
 for (let i = 90; i >= 0; i -= step) {
@@ -37,9 +43,12 @@ for (let i = 90; i >= 0; i -= step) {
   )
 }
 return (
-      <div class="flex space-y-2">
+      <div class="flex space-y-2 items-center">
+      <div class="font-semibold">
+      {hue}
+      </div>
       <For each={shades}>
-      {shade => <ColorSwatch className={`bg-${hue}-${shade}`}/> }
+      {shade => <ColorSwatch className={`bg-${hue}-${shade}`} showShade={showShade}/> }
       </For>
       </div>
 )
--- src/utils/design-system.tsx
+++ src/utils/design-system.tsx
@@ -27,18 +27,15 @@ const genShades = (
   return shades;
 };
 
-const genShadesV2 = (hue: number, options?: {saturationAdjust?: number}): Record<number, string> => {
+const genShadesV2 = (hue: number, options?: {saturation?: number}): Record<number, string> => {
   const shades: Record<number, string> = {};
   const distanceFromDarkestBlue = Math.abs((hue - 243) % 360)
-  const maxDarkness = 10 - distanceFromDarkestBlue / 40
+  const maxDarkness = 10 - distanceFromDarkestBlue / 50
   for (let i = 0; i <= 90; i = i + 5) {
     // const lightness = -1 * Math.pow((i / 100) - 0.5, 2) + 0.5
     const lightness = maxDarkness + i / 90 * (90 - maxDarkness)
-    let saturation = 70
-    if (options?.saturationAdjust) {
-      saturation += options.saturationAdjust
-    }
-  shades[i] = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+    const saturation = options?.saturation ?? 70
+    shades[i] = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
   }
   return shades;
 }
@@ -61,15 +58,15 @@ const genGrays = (
 };
 export const gray = genGrays(grayHue, 8, 5);
 
-const blue = genShadesV2(210);
+const blue = genShadesV2(198);
 const teal = genShadesV2(180);
 const primary = blue;
-const yellow = genShadesV2(41, {saturationAdjust: 20});
-const orange = genShadesV2(24, {saturationAdjust: 10});
+const yellow = genShadesV2(40, {saturation: 90});
+const orange = genShadesV2(24, {saturation: 80});
 const pink = genShadesV2(308);
 const purple = genShadesV2(271);
 const red = genShadesV2(350);
-const green = genShadesV2(118, {saturationAdjust: -10});
+const green = genShadesV2(130, {saturation: 70});
 const success = genShadesV2(164);
 const arrows = genShadesV2(40);
 
