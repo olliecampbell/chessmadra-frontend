1d92f69b4a6e2b044ab28894c7c5f72d40ff888d
Marcus Buffett <me@mbuffett.com>
2023-08-31 23:26:53 +0200

    .

---
+++ src/components/HomePageCTA.tsx
@@ -0,0 +1,42 @@
+import { clsx } from "~/utils/classes";
+import { createScrollPosition } from "@solid-primitives/scroll";
+import { getFeatureLoaded } from "~/utils/experiments";
+import { Match, Switch } from "solid-js";
+
+export const HomePageCTA = (props: { onClick: () => void }) => {
+  const ctas = () => {
+    const feature = getFeatureLoaded("homepage-cta");
+    if (feature === "1") {
+      return "Get started";
+    } else if (feature === "2") {
+      return "Start building";
+    } else {
+      return "Try it for free";
+    }
+  };
+  return (
+    <div>
+      <Switch
+        fallback={
+          <div
+            class={clsx(
+              "px-[18px] lg:px-[24px] py-[9px] lg:py-[12px] flex justify-center items-center bg-gray-90 transition-all hover:bg-orange-60 rounded",
+            )}
+          >
+            Try it for free
+          </div>
+        }
+      >
+        <Match when={!!getFeatureLoaded("homepage-cta")}>
+          <div
+            class={clsx(
+              "px-[18px] lg:px-[24px] py-[9px] lg:py-[12px] flex justify-center items-center bg-green-40 transition-all hover:bg-green-50 rounded text-gray-95",
+            )}
+          >
+            {ctas()}
+          </div>
+        </Match>
+      </Switch>
+    </div>
+  );
+};
