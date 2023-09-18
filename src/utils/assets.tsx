489f72fb4c1856cac2d0e6b89f1b07599c3700a8
Marcus Buffett <me@mbuffett.com>
2023-09-19 01:00:58 +0200

    .

--- src/utils/assets.tsx
+++ src/utils/assets.tsx
@@ -1,7 +1,11 @@
 import { BASE_FRONTEND_URL } from "./base_url";
 
 export const getStatic = (path: string) => {
-	return `${BASE_FRONTEND_URL}${path}`;
+	if (BASE_FRONTEND_URL) {
+		return `${BASE_FRONTEND_URL}${path}`;
+	} else {
+		return path;
+	}
 	// evenually
 	// return `${path}?ver=${process.env.FRONTEND_VERSION}`;
 };
