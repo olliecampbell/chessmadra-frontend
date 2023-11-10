68570ce73da0bad1f261fc3cf071c5af8309d430
Marcus Buffett <me@mbuffett.com>
2023-11-10 13:00:39 -0500

    more android stuff

--- android/app/src/main/java/com/chessbook/MainActivity.java
+++ android/app/src/main/java/com/chessbook/MainActivity.java
@@ -1,5 +1,15 @@
 package com.chessbook;
+import android.os.Bundle;
+
+import android.webkit.WebView;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView.setWebContentsDebuggingEnabled(true);
+    }
+
+}
